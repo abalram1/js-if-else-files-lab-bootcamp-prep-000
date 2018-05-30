7c219c03f347859012e0ab40919d2bca18fd6a01
Anil Balram <adbalramd1@gmail.com>
2018-05-30 21:37:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-var name = "Susan"
+var name = "Joe"
+var name2 = "Susan"
 var height = "74"
 
-if (name === "Susan") {
+if (name2 === "Susan") {
    console.log("The name is correct")
 }
   else {
    console.log("Expected the name to be Susan")
 }
+
+if (name === "Joe") {
+   console.log("The name is correct")
+}
+  else {
+    console.log("I thought the name was something else")
+  }
+  
 // Don't worry about this
 module.exports = { name, height
 }
