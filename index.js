44c8239aea579fcdf8c48d244f55496582ad6c4a
Anil Balram <adbalramd1@gmail.com>
2018-05-30 21:38:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 var name = "Joe"
-var name2 = "Susan"
 var height = "74"
 
-if (name2 === "Susan") {
-   console.log("The name is correct")
-}
-  else {
-   console.log("Expected the name to be Susan")
-}
 
 if (name === "Joe") {
    console.log("The name is correct")
